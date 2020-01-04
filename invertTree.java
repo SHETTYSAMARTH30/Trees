2df69b0da1b4f2ad146776892513e2cf4f8aeeb7
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-01-03 23:26:35 -0800

    Create invertTree.java

---
+++ invertTree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class invertTree {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        
+        List<Double> sum=new ArrayList<>();
+        List<Integer> count=new ArrayList<>();
+        
+        averageLevel(root,0,sum,count);
+        
+        for(int i=0;i<sum.size();i++){
+            sum.set(i,sum.get(i) / count.get(i));
+        }
+        
+        return sum;
+    }
+    
+    public void averageLevel(TreeNode root,int level,List<Double> sum,List<Integer> count){
+        
+        if(root == null)
+            return;
+        
+        if(level<sum.size()){
+            sum.set(level,sum.get(level)+root.val);
+            count.set(level,count.get(level)+1);
+        }
+        else{
+            sum.add(root.val*1.0);
+            count.add(1);
+        }
+        
+        averageLevel(root.left,level+1,sum,count);
+        averageLevel(root.right,level+1,sum,count);
+    }
+}
